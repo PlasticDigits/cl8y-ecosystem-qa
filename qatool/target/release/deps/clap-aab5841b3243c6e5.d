ce32e3aa94e1c7c411ceb12d5ed4729eb62a615c
brouie <liminalsolana@gmail.com>
2026-03-17 06:01:26 +0100

    chore: add .gitignore, remove build artifacts

--- qatool/target/release/deps/clap-aab5841b3243c6e5.d
+++
@@ -1,9 +0,0 @@
-/root/cl8y-ecosystem-qa/qatool/target/release/deps/clap-aab5841b3243c6e5.d: /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/clap-4.6.0/src/lib.rs /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/clap-4.6.0/src/../examples/demo.rs /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/clap-4.6.0/src/../examples/demo.md
-
-/root/cl8y-ecosystem-qa/qatool/target/release/deps/libclap-aab5841b3243c6e5.rlib: /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/clap-4.6.0/src/lib.rs /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/clap-4.6.0/src/../examples/demo.rs /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/clap-4.6.0/src/../examples/demo.md
-
-/root/cl8y-ecosystem-qa/qatool/target/release/deps/libclap-aab5841b3243c6e5.rmeta: /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/clap-4.6.0/src/lib.rs /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/clap-4.6.0/src/../examples/demo.rs /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/clap-4.6.0/src/../examples/demo.md
-
-/root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/clap-4.6.0/src/lib.rs:
-/root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/clap-4.6.0/src/../examples/demo.rs:
-/root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/clap-4.6.0/src/../examples/demo.md:
--- qatool/target/release/qatool.d
+++
@@ -1,1 +0,0 @@
-/root/cl8y-ecosystem-qa/qatool/target/release/qatool: /root/cl8y-ecosystem-qa/qatool/src/config.rs /root/cl8y-ecosystem-qa/qatool/src/main.rs /root/cl8y-ecosystem-qa/qatool/src/template.rs
